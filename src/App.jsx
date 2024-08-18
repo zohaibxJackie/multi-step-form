647be8a3bd098b5469a9aaeac58b17634eebd57f
zohaibxJackie <zkhan52607@gmail.com>
2024-08-18 19:33:10 +0500

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import SecondPage from "./components/SecondPage";
 import ThirdPage from "./components/ThirdPage";
 import FourthPage from "./components/FourthPage";
 
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className='font-ff-main mt-8'>
       <div className="flex max-w-[70%] p-2 mx-auto border rounded shadow-md h-[585px]">
         <LeftPanel />
-        <FirstPage />
+        <Routes>
+          <Route index element={<FirstPage />} />
+          <Route path="/select-plan" element={<SecondPage />} />
+          <Route path="/add-ons" element={<ThirdPage />} />
+          <Route path="/final-step" element={<FourthPage />} />
+        </Routes>
       </div>
     </div>
   )
