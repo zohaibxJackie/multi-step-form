8d70639cda3f0d702fb72b6cdb688987264d7c90
zohaibxJackie <zkhan52607@gmail.com>
2024-08-30 12:01:58 +0500

    routing bug fiexed

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react'
 import DesktopImage from '../assets/images/bg-sidebar-desktop.svg'
 import MobileImage from '../assets/images/bg-sidebar-mobile.svg'
 
 const LeftPanel = (props) => {
   const {activeButtonBg} = props
+  useEffect(() => {
+    console.log(activeButtonBg);
+  
+  }, [activeButtonBg])
+  
+  
 
   return (
     <aside className="text-white relative rounded-md md:fixed md:min-w-full md:top-0 md:left-0">
@@ -10,7 +17,7 @@ const LeftPanel = (props) => {
       <img src={MobileImage} alt="Background Image" className='hidden md:block w-full h-[10rem] object-cover object-center' />
       <div className="absolute top-[10%] left-[10%] md:top-[35%] md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] flex flex-col gap-4 md:flex-row md:justify-center md:items-center">
         <div className="flex gap-4 items-center">
-          <span className={`w-10 h-10 ${activeButtonBg === '/multi-step-form' ? 'bg-Light-blue text-marine-blue' : 'text-white'} border-Pastel-blue border  rounded-full flex items-center justify-center`}>1</span>
+          <span className={`w-10 h-10 ${activeButtonBg === '/multi-step-form/' ? 'bg-Light-blue text-marine-blue' : 'text-white'} border-Pastel-blue border  rounded-full flex items-center justify-center`}>1</span>
           <div className="ml-1 md:hidden">
             <p className="text-Cool-gray text-sm uppercase">Step 1</p>
             <p className="uppercase">Your info</p>
